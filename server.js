71b6888ad56bf89664b0641ae4cad6d71275788f
njguernsey <asparagusbunny@gmail.com>
2026-03-26 14:50:31 -0700

    Fix broccoli page routing and API path

--- server.js
+++ server.js
@@ -28,4 +28,7 @@ app.post('/api/chat', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static('../'));
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
