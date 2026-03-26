c4b2f517faff2a59edb8bff817cc34c6e4f7a6ee
njguernsey <asparagusbunny@gmail.com>
2026-03-26 15:35:49 -0700

    Add request logging to debug 405 error

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 const broccoliQA = [
   {
     id: "broccoli-nutrition-overview",
